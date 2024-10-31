fb7d24786ea13431706a13b40739eae9706011dc
Erunoma <oliver.boots@hotmail.com>
2024-10-31 10:11:32 +0100

    other commit

---
+++ Assets/EquipConfirm.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EquipConfirm : MonoBehaviour {
+
+	public GameObject selectedWeapon;
+	public Image weaponIcon;
+	public Text dpsIncrease;
+	public Text newDps;
+	public GameObject playerManager;
+	public GameObject equipPage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		weaponIcon.sprite = selectedWeapon.GetComponent<Image> ().sprite;
+		dpsIncrease.text = selectedWeapon.GetComponent<SwordProperties> ().dps.ToString();
+		newDps.text = playerManager.GetComponent<PlayerStatData> ().dps + selectedWeapon.GetComponent<SwordProperties> ().dps.ToString ();
+	}
+
+	public void OnConfirm(){
+		equipPage.GetComponent<EquipPage> ().equippedWeapon = selectedWeapon;
+		equipPage.GetComponent<EquipPage> ().choosingWeapon = false;
+		equipPage.GetComponent<EquipPage> ().weaponEquipped = true;
+
+	}
+}
---
+++ Assets/EquipPage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipPage : MonoBehaviour {
+
+	public GameObject inventory;
+	public bool choosingWeapon;
+	public bool weaponEquipped;
+
+	public GameObject equippedWeapon;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void InitWeaponEquip(){
+		inventory.SetActive (true);
+		choosingWeapon = true;
+	}
+	public void OnWeaponSelect(){
+	}
+}
